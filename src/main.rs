b4512803502202c99186819e87400342008a97fd
Travis Perdue <perduetravis@gmail.com>
2018-01-25 23:26:30 -0600

    finish chapter 6

--- src/main.rs
+++ src/main.rs
@@ -39,6 +39,23 @@ enum Message {
     ChangeColor(i32, i32, i32)
 }
 
+
+#[derive(Debug)] // So we can inspect the state
+enum UsState {
+    Alabama,
+    Alaska,
+    // ... etc
+}
+
+// use to explore Rust match
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+
 // example of using structs for the various data variants from the
 // Message enum
 // problem is that it isn't easy to create one function that accepts
@@ -96,8 +113,74 @@ fn main() {
     let x: i8 = 5;
     let y: Option<i8> = Some(5);
 
+    let penny = Coin::Penny;
+    let cents = value_in_cents(penny);
+    println!("a penny's value is: {}", cents);
+
+    let quarter = Coin::Quarter(UsState::Alaska);
+    let cents = value_in_cents(quarter);
+
     // this will not compile essentially because you have to get the <T> out of Option<T>
     // before you can operate it so i8 + Option<i8> doesn't work.
     // let sum = x + y;
+
+    // examples of pattern matching with Option<T>
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("some {:?}", five);
+    println!("none {:?}", none);
+
+    // dealing with some cases but not all by using _
+    // without _ Rust expects an exhaustive match
+    let some_u8_value: u8 = 0;  
+
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => ()
+    }
+
+    let some_u8_value = Some(3u8);
+    // match only one value 
+    match some_u8_value {
+        Some(3) => println!("3"),
+        _ => ()
+    }
+
+    let some_u8_value = Some(0u8);
+    // same but more concise for matching only 1 value 
+    if let Some(3) = some_u8_value {
+        println!("3");
+    }
+
+    // can include an else that is comparable
+    // to the code in the _ branch of a match expression
+    if let Some(3) = some_u8_value {
+        println!("3");
+    } else {
+        println!("not 3");
+    }
+
 }
 
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) =>{
+            println!("State quarter from {:?}", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
